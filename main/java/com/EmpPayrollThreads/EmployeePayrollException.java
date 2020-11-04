030745e4d48c59ff0e3bc4e96682187e187af0fa
suyash-dce <jain.suyash28@gmail.com>
2020-11-04 17:18:16 +0530

    Added: Custom Exception Class Added to throw User Defined Exceptions

---
+++ main/java/com/EmpPayrollThreads/EmployeePayrollException.java
@@ -0,0 +1,12 @@
+public class EmployeePayrollException extends Exception {
+
+	public enum ExceptionType
+	{UPDATE_FAIL}
+
+	public ExceptionType type;
+
+	public EmployeePayrollException(ExceptionType type,String message) {
+		super(message);
+		this.type = type;
+	}
+}
